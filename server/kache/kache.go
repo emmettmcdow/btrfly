bd60ac08a98e60eeca1983062fc2c5e951a86914
Emmett McDow <emmett.mcdow@gmail.com>
2024-06-01 16:00:28 -0700

    Code complete for Kache memory backend

--- server/kache/kache.go
+++ server/kache/kache.go
@@ -6,11 +6,26 @@ import (
 
 type Artifact struct {
 	Hash   uint64
-	Length uint64
 	Data   []byte
 }
 
-func (a Artifact) Equal(b Artifact) bool {
+type Tag struct {
+	// Name string
+	Artifacts map[string]*Artifact
+}
+
+type User struct {
+	ID uint64
+	Tags map[string]*Tag
+}
+
+type Handler interface {
+	GetArtifact(url string, id string, userID uint64) (artifact *Artifact, err error)
+	AddArtifact(artifact *Artifact, url string, id string, userID uint64) (err error)
+	AddUser(user *User)
+}
+
+func (a *Artifact) Equal(b *Artifact) bool {
 	if a.Hash != b.Hash {
 		return false
 	}
@@ -21,10 +36,12 @@ func (a Artifact) Write(p []byte) (n int, err error) {
 	return
 }
 
-func GetArtifact(url string, id string) (artifact Artifact, err error) {
-	return
-}
+func CreateUser() (user *User) {
+	// TODO: for now we only have an id of 0
+	var id uint64
+	id = 0
+	tags := make(map[string]*Tag)
 
-func AddArtifact(artifact Artifact, url string, id string) {
-	return
+	user = &User{ID: id, Tags: tags}
+	return user
 }
---
+++ server/kache/memory.go
@@ -0,0 +1,66 @@
+package kache
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Implements kache.Handler
+// For testing
+type Memory struct {
+	Artifacts []*Artifact
+	Users     []*User
+}
+
+func (m *Memory) AddUser(user *User) {
+	m.Users = append(m.Users, user)
+}
+
+func (m *Memory) GetArtifact(url string, tagID string, userID uint64) (artifact *Artifact, err error) {
+	user := m.Users[userID]
+	if user == nil {
+		return artifact, errors.New(fmt.Sprintf("Failed to get user with ID: %d", userID))
+	}
+	tag, ok := user.Tags[tagID]
+	if !ok {
+		return artifact, errors.New(fmt.Sprintf("Failed to get tag with ID: %d", tagID))
+	}
+	artifact, ok = tag.Artifacts[url]
+	if !ok {
+		return artifact, errors.New(fmt.Sprintf("Failed to get artifact for URL: %s", url))
+	}
+	return artifact, nil
+}
+
+func (m *Memory) AddArtifact(artifact *Artifact, url string, tagID string, userID uint64) (err error){
+	user := m.Users[userID]
+	if user == nil {
+		return errors.New(fmt.Sprintf("Failed to get user with ID: %d", userID))
+	}
+	tag, ok := user.Tags[tagID]
+	if !ok {
+		tag = &Tag{Artifacts: make(map[string]*Artifact)}
+		user.Tags[tagID] = tag
+	}
+
+	existingArtifact := false
+	for _, currArtifact := range tag.Artifacts {
+		if artifact.Equal(currArtifact) {
+			existingArtifact = true
+			break
+		}
+	}
+
+	if !existingArtifact {
+		m.Artifacts = append(m.Artifacts, artifact)
+	}
+	tag.Artifacts[url] = artifact
+	return nil
+}
+
+func CreateMemory() (m *Memory){
+	a := make([]*Artifact, 1)
+	u := make([]*User, 1)
+	m = &Memory{Artifacts: a, Users: u}
+	return m
+}
