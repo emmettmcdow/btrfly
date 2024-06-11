87716a6d92041536931e26358e8291d7ff34be70
Emmett McDow <emmett.mcdow@gmail.com>
2024-06-11 11:32:23 -0400

    Clean up the CLI

--- client/client.go
+++ client/client.go
@@ -16,31 +16,26 @@ var ctrlEndpoint string
 var proxyEndpoint string
 
 const	defaultCtrlEndpoint string = "127.0.0.1:81"
-	// defaultProxyEndpoint := "127.0.0.1:80"
 const	defaultDNSEndpoint string =  "127.0.0.1:53"
+
 func main() {
+	var subcommand string
 	client = &http.Client{}
 	d = dns.ConfigAgent()
 
-
-	// configCmd := flag.NewFlagSet("config", flag.ExitOnError)
-	
-	// deconfigCmd := flag.NewFlagSet("deconfig", flag.ExitOnError)
-
-	// modeCmd := flag.NewFlagSet("mode", flag.ExitOnError)
-
-	// loginCmd := flag.NewFlagSet("login", flag.ExitOnError)
-	
-	// tagCmd := flag.NewFlagSet("tag", flag.ExitOnError)
-
 	flag.Parse()
 	arglen := len(flag.Args())
-	switch os.Args[1] {
+	if arglen == 0 {
+		subcommand = "help"
+	} else {
+		subcommand = flag.Args()[0]
+	}
+	switch subcommand {
 	case "config":
 		var dnsEndpoint string
-		if arglen == 1 {
-			dnsEndpoint = flag.Args()[0]
-		} else if arglen == 0 {
+		if arglen == 2 {
+			dnsEndpoint = flag.Args()[1]
+		} else if arglen == 1 {
 			dnsEndpoint = defaultDNSEndpoint
 		} else {
 			fmt.Fprintf(os.Stderr, "Unrecognized arguments.\n")
@@ -49,79 +44,83 @@ func main() {
 		d.Config(dnsEndpoint)
 		d.FlushCache()
 	case "deconfig":
-		if arglen != 0 {
+		if arglen != 1 {
 			fmt.Fprintf(os.Stderr, "Unrecognized arguments.\n")
 			os.Exit(1)
 		}
 		d.Deconfig()
 		d.FlushCache()
 	case "tag":
-		if arglen != 1 {
+		if arglen != 2 {
 			fmt.Fprintf(os.Stderr, "No tag given.\n")
 			os.Exit(1)
 		}
-		if err := tag(flag.Args()[0]); err != nil {
+		if err := tag(flag.Args()[1]); err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to set the tag: %s\n\n", err)
 			os.Exit(1)
 		}
 	case "login":
-		if arglen != 1 {
+		if arglen != 2 {
 			fmt.Fprintf(os.Stderr, "No login id given.\n")
 			os.Exit(1)
 		}
-		if err := login(flag.Args()[0]); err != nil {
+		if err := login(flag.Args()[1]); err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to login: %s\n\n", err)
 			os.Exit(1)
 		}
 	case "mode":
-		if arglen != 1 {
+		if arglen != 2 {
 			fmt.Fprintf(os.Stderr, "No mode given.\n")
 			os.Exit(1)
 		}
-		if err := mode(flag.Args()[0]); err != nil {
+		if err := mode(flag.Args()[1]); err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to set mode: %s\n\n", err)
 			os.Exit(1)
 		}
 	case "help":
-		if arglen != 1 {
-			// TODO print default message
-			fmt.Fprintf(os.Stderr, "No mode given.\n")
-			os.Exit(1)
-		}
-		// TODO: format better
-		// TODO: figure out standard unix format for specifying commandlines
-		switch flag.Args()[1]{
-		case "config":
-			fmt.Printf("Help: kache config [dns server]\n")
-			fmt.Printf("    config - configure this machine to utilize the Kache server\n")
-			fmt.Printf("    Takes an optional argument [dns server]. This overrides the default dns\n")
-			fmt.Printf("    server\n")
-		case "deconfigure":
-			fmt.Printf("Help: kache deconfigure\n")
-			fmt.Printf("    deconfigure - unsets the dns server set by config\n")
-		case "tag":
-			fmt.Printf("Help: kache tag <tag_name>")
-			fmt.Printf("    tag - set the tag to identify this current build\n")
-			fmt.Printf("    tag_name is required and passed as an argument\n")
-		case "mode":
-			fmt.Printf("Help: kache mode <mode_verb>")
-			fmt.Printf("    mode - change the mode of operation of the kache service\n")
-			fmt.Printf("    mode_verb is required and passed as an argument\n")
-			fmt.Printf("    mode_verb is one of: record, playback, standby\n")
-		case "login":
-			fmt.Printf("Help: kache login <id>")
-			fmt.Printf("    login - set your credentials so that you can use the Kache service\n")
-			fmt.Printf("    id is required and passed as an argument\n")
+		if arglen == 2 {
+			switch flag.Args()[1]{
+			case "config":
+				fmt.Printf("Help: kache config [dns_server]\n")
+				fmt.Printf("    config - configure this machine to utilize the Kache server\n")
+				fmt.Printf("    Takes an optional argument [dns server]. This overrides the default dns\n")
+				fmt.Printf("    server\n")
+			case "deconfigure":
+				fmt.Printf("Help: kache deconfigure\n")
+				fmt.Printf("    deconfigure - unsets the dns server set by config\n")
+			case "tag":
+				fmt.Printf("Help: kache tag tag_name")
+				fmt.Printf("    tag - set the tag to identify this current build\n")
+				fmt.Printf("    tag_name is required and passed as an argument\n")
+			case "mode":
+				fmt.Printf("Help: kache mode mode_ver")
+				fmt.Printf("    mode - change the mode of operation of the kache service\n")
+				fmt.Printf("    mode_verb is required and passed as an argument\n")
+				fmt.Printf("    mode_verb is one of: record, playback, standby\n")
+			case "login":
+				fmt.Printf("Help: kache login id")
+				fmt.Printf("    login - set your credentials so that you can use the Kache service\n")
+				fmt.Printf("    id is required and passed as an argument\n")
+			}
+		} else {
+			defaultHelp()
+			os.Exit(0)
 		}
 	default:
-		fmt.Printf("%s is not a valid subcommand", os.Args[1])
+		fmt.Printf("%s is not a valid subcommand", subcommand)
+		defaultHelp()
+	}
+}
+
+func defaultHelp() {
+		fmt.Printf("Kache Client CLI\n")
 		fmt.Printf("Available subcommands:\n")
+		fmt.Printf("    config - configure this machine to utilize the Kache server\n")
 		fmt.Printf("    deconfigure - deconfigure this machine (...)\n")
 		fmt.Printf("    tag         - set the tag to identify this current build\n")
 		fmt.Printf("    login       - set your credentials so that you can use the Kache service\n")
 		fmt.Printf("    mode        - change the mode of operation of the kache service\n")
 		fmt.Printf("    help        - pass another subcommand to get info about that subcommand\n")
-	}
 }
 
 func tag(tag string) (err error) {
