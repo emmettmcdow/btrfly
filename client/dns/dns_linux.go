b91c5e91928b4af778f55199e9b5d2f301f2b307
Emmett McDow <emmett.mcdow@gmail.com>
2024-07-01 17:53:17 -0700

    Fixed some Linux (found possible race condition?)

--- client/dns/dns_linux.go
+++ client/dns/dns_linux.go
@@ -21,19 +21,19 @@ func Config(ip string) (err error) {
 	cmd1.Stderr = &stderr
 	err = cmd1.Run()
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to run [tee /etc/resolv.conf...]. Stderr: %s", stderr.String())
 	}
 	cmd2.Stdout = &out
 	cmd2.Stderr = &stderr
 	err = cmd2.Run()
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to run [mv resolv.conf -> tmp...]. Stderr: %s", stderr.String())
 	}
 	cmd3.Stdout = &out
 	cmd3.Stderr = &stderr
 	err = cmd3.Run()
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to run [ln -s resolv-manual resolv...]. Stderr: %s", stderr.String())
 	}
 
 	return nil
@@ -41,8 +41,8 @@ func Config(ip string) (err error) {
 
 func Deconfig() (err error) {
 	// TODO: don't do this with bash lmao
-	var cmd1 = exec.Command("/bin/sh", "-c", fmt.Sprintf("mv %s /etc/resolv.conf", TMPFILE))
-	var cmd2 = exec.Command("/bin/sh", "-c", "rm /etc/resolv-manual.conf")
+	var cmd1 = exec.Command("/bin/sh", "-c", fmt.Sprintf("sudo mv %s /etc/resolv.conf", TMPFILE))
+	var cmd2 = exec.Command("/bin/sh", "-c", "sudo rm /etc/resolv-manual.conf")
 	var out strings.Builder
 	var stderr strings.Builder
 
@@ -50,18 +50,31 @@ func Deconfig() (err error) {
 	cmd1.Stderr = &stderr
 	err = cmd1.Run()
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to run [mv tmp-resolv -> resolv...]. Stderr: %s", stderr.String())
 	}
 	cmd2.Stdout = &out
 	cmd2.Stderr = &stderr
 	err = cmd2.Run()
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to run [rm resolv-manual...]. Stderr: %s", stderr.String())
 	}
 
 	return nil
 }
 
 func FlushCache() (err error) {
-	return nil // TODO: implement cache flushing
+	// sudo resolvectl flush-caches
+
+	// var cmd1 = exec.Command("/bin/sh", "-c", "sudo resolvectl flush-caches")
+	// var out strings.Builder
+	// var stderr strings.Builder
+
+	// cmd1.Stdout = &out
+	// cmd1.Stderr = &stderr
+	// err = cmd1.Run()
+	// if err != nil {
+	// 	return fmt.Errorf("Failed to run [mv tmp-resolv -> resolv...]. Stderr: %s", stderr.String())
+	// }
+
+	return nil
 }
