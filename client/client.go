1679781cacba1cb92eb7f4875746f0243c9c9f6f
Emmett McDow <emmett.mcdow@gmail.com>
2024-07-07 20:51:06 -0700

    Adds tests for client

--- client/client.go
+++ client/client.go
@@ -1,90 +1,114 @@
 package main
 
 import (
-	"net/http"
-	"net/url"
-	"os"
-	"fmt"
-	"io"
 	"flag"
+	"fmt"
 	"github.com/emmettmcdow/kache/client/dns"
+	"io"
+	"net/http"
+	"os"
 )
 
 var client *http.Client
 var ctrlEndpoint string
 var proxyEndpoint string
 
-const	defaultCtrlEndpoint string = "127.0.0.1:81"
-const	defaultDNSEndpoint string =  "127.0.0.1:53"
+const defaultCtrlEndpoint string = "127.0.0.1:81"
+const defaultDNSEndpoint string = "127.0.0.1:53"
+
+type defaultDns struct{}
+
+func (d defaultDns) Config(ip string) (err error) {
+	return dns.Config(ip)
+}
+func (d defaultDns) Deconfig() (err error) {
+	return dns.Deconfig()
+}
+func (d defaultDns) FlushCache() (err error) {
+	return dns.FlushCache()
+}
+
+type DNSConfig interface {
+	Config(ip string) (err error)
+	Deconfig() (err error)
+	FlushCache() (err error)
+}
 
 func main() {
+	flag.Parse()
+	args := flag.Args()
+	arglen := len(flag.Args())
+	out := _main(defaultDns{}, defaultCtrlEndpoint, arglen, args)
+	os.Exit(out)
+}
+
+// TODO: pass config and deconfig errors back up!
+func _main(dns DNSConfig, ctrlEndpoint string, arglen int, args []string) int {
 	var subcommand string
 	client = &http.Client{}
 
-	flag.Parse()
-	arglen := len(flag.Args())
 	if arglen == 0 {
 		subcommand = "help"
 	} else {
-		subcommand = flag.Args()[0]
+		subcommand = args[0]
 	}
 	switch subcommand {
 	case "config":
 		var dnsEndpoint string
 		if arglen == 2 {
-			dnsEndpoint = flag.Args()[1]
+			dnsEndpoint = args[1]
 		} else if arglen == 1 {
 			dnsEndpoint = defaultDNSEndpoint
 		} else {
 			fmt.Fprintf(os.Stderr, "Unrecognized arguments.\n")
-			os.Exit(1)
+			return 1
 		}
 		dns.Config(dnsEndpoint)
 		dns.FlushCache()
 	case "deconfig":
 		if arglen != 1 {
 			fmt.Fprintf(os.Stderr, "Unrecognized arguments.\n")
-			os.Exit(1)
+			return 1
 		}
 		dns.Deconfig()
 		dns.FlushCache()
 	case "tag":
 		if arglen != 2 {
 			fmt.Fprintf(os.Stderr, "No tag given.\n")
-			os.Exit(1)
+			return 1
 		}
-		if err := tag(flag.Args()[1]); err != nil {
+		if err := tag(args[1], ctrlEndpoint); err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to set the tag: %s\n\n", err)
-			os.Exit(1)
+			return 1
 		}
 	case "login":
 		if arglen != 2 {
 			fmt.Fprintf(os.Stderr, "No login id given.\n")
-			os.Exit(1)
+			return 1
 		}
-		if err := login(flag.Args()[1]); err != nil {
+		if err := login(args[1], ctrlEndpoint); err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to login: %s\n\n", err)
-			os.Exit(1)
+			return 1
 		}
 	case "mode":
 		if arglen != 2 {
 			fmt.Fprintf(os.Stderr, "No mode given.\n")
-			os.Exit(1)
+			return 1
 		}
-		if err := mode(flag.Args()[1]); err != nil {
+		if err := mode(args[1], ctrlEndpoint); err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to set mode: %s\n\n", err)
-			os.Exit(1)
+			return 1
 		}
 	case "help":
 		if arglen == 2 {
-			switch flag.Args()[1]{
+			switch args[1] {
 			case "config":
 				fmt.Printf("Help: kache config [dns_server]\n")
 				fmt.Printf("    config - configure this machine to utilize the Kache server\n")
 				fmt.Printf("    Takes an optional argument [dns server]. This overrides the default dns\n")
 				fmt.Printf("    server\n")
-			case "deconfigure":
-				fmt.Printf("Help: kache deconfigure\n")
+			case "deconfig":
+				fmt.Printf("Help: kache deconfig\n")
 				fmt.Printf("    deconfigure - unsets the dns server set by config\n")
 			case "tag":
 				fmt.Printf("Help: kache tag tag_name")
@@ -102,30 +126,33 @@ func main() {
 			}
 		} else {
 			defaultHelp()
-			os.Exit(0)
+			return 0
 		}
 	default:
 		fmt.Printf("%s is not a valid subcommand", subcommand)
 		defaultHelp()
+		return 1
 	}
+	return 0
 }
 
 func defaultHelp() {
-		fmt.Printf("Kache Client CLI\n")
-		fmt.Printf("Available subcommands:\n")
-		fmt.Printf("    config - configure this machine to utilize the Kache server\n")
-		fmt.Printf("    deconfigure - deconfigure this machine (...)\n")
-		fmt.Printf("    tag         - set the tag to identify this current build\n")
-		fmt.Printf("    login       - set your credentials so that you can use the Kache service\n")
-		fmt.Printf("    mode        - change the mode of operation of the kache service\n")
-		fmt.Printf("    help        - pass another subcommand to get info about that subcommand\n")
+	fmt.Printf("Kache Client CLI\n")
+	fmt.Printf("Available subcommands:\n")
+	fmt.Printf("    config - configure this machine to utilize the Kache server\n")
+	fmt.Printf("    deconfigure - deconfigure this machine (...)\n")
+	fmt.Printf("    tag         - set the tag to identify this current build\n")
+	fmt.Printf("    login       - set your credentials so that you can use the Kache service\n")
+	fmt.Printf("    mode        - change the mode of operation of the kache service\n")
+	fmt.Printf("    help        - pass another subcommand to get info about that subcommand\n")
 }
 
-func tag(tag string) (err error) {
-	req := &http.Request{Host: defaultCtrlEndpoint,
-						URL:    &url.URL{Path: "/tag"},
-						Method: "GET",
-						Header: http.Header{"Tag": []string{tag}}}
+func tag(tag string, ctrlEndpoint string) (err error) {
+	req, err := http.NewRequest("GET", "http://"+ctrlEndpoint+"/tag", http.NoBody)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Tag", tag)
 	resp, err := client.Do(req)
 	if err != nil {
 		return fmt.Errorf("Failed to perform http request: %s", err)
@@ -140,11 +167,12 @@ func tag(tag string) (err error) {
 	return nil
 }
 
-func login(id string) (err error) {
-	req := &http.Request{Host: defaultCtrlEndpoint,
-						URL:    &url.URL{Path: "/login"},
-						Method: "GET",
-						Header: http.Header{"ID": []string{id}}}
+func login(id string, ctrlEndpoint string) (err error) {
+	req, err := http.NewRequest("GET", "http://"+ctrlEndpoint+"/login", http.NoBody)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Id", id)
 	resp, err := client.Do(req)
 	if err != nil {
 		return fmt.Errorf("Failed to perform http request: %s", err)
@@ -159,7 +187,7 @@ func login(id string) (err error) {
 	return nil
 }
 
-func mode(mode string) (err error) {
+func mode(mode string, ctrlEndpoint string) (err error) {
 	var modeI string
 	switch mode {
 	case "record":
@@ -171,10 +199,11 @@ func mode(mode string) (err error) {
 	default:
 		return fmt.Errorf("Mode '%s' is not a valid mode.", mode)
 	}
-	req := &http.Request{Host: defaultCtrlEndpoint,
-						URL:    &url.URL{Path: "/mode"},
-						Method: "GET",
-						Header: http.Header{"Mode": []string{modeI}}}
+	req, err := http.NewRequest("GET", "http://"+ctrlEndpoint+"/mode", http.NoBody)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Mode", modeI)
 	resp, err := client.Do(req)
 	if err != nil {
 		return fmt.Errorf("Failed to perform http request: %s", err)
