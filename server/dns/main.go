8ac1f6a5b50c30fe51925c6e23ff3d8a9c3a722a
Emmett McDow <emmett.mcdow@gmail.com>
2024-08-07 20:21:36 -0700

    Got DNS server working

---
+++ server/dns/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+const DEFAULT_ADDR = "127.0.0.1:53"
+
+var DEFAULT_RESPONSE = []byte{127, 0, 0, 1}
+
+func dns(listen string) {
+	// Resolve the string address to a UDP address
+	udpAddr, err := net.ResolveUDPAddr("udp", listen)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Start listening for UDP packages on the given address
+	conn, err := net.ListenUDP("udp", udpAddr)
+	fmt.Printf("DNS server listening on %s\n", DEFAULT_ADDR)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for {
+		var buf [512]byte
+		_, client, err := conn.ReadFromUDP(buf[0:])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		response, err := Deserialize(buf[0:])
+		if err != nil {
+			fmt.Printf("Failed to deserialize message: %s", err)
+			continue
+		}
+		response.header.ancount = 1
+		qr, opcode, aa, tc, rd, ra, z, rcode := unpacked(response.header.packed)
+		qr = 1
+		ra = 1
+		response.header.packed = packed(qr, opcode, aa, tc, rd, ra, z, rcode)
+
+		answer := Answer{
+			name:     response.questions[0].qname,
+			kind:     response.questions[0].qtype,
+			class:    response.questions[0].qclass,
+			ttl:      420,
+			rdlength: 4,
+			rdata:    DEFAULT_RESPONSE,
+		}
+		response.answers = append(response.answers, answer)
+		data, err := Serialize(response)
+		if err != nil {
+			fmt.Printf("Failed to serialize message: %s", err)
+			continue
+		}
+		conn.WriteToUDP(data, client)
+	}
+}
+
+func main() {
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	dns(DEFAULT_ADDR)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			if sig == syscall.SIGINT {
+				log.Println("Recieved keyboard interrupt. Shutting down server.")
+				break
+			}
+		}
+	}()
+
+	wg.Wait()
+}
