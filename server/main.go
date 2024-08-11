189ec02cdbf2b69d186145e64c2cc534c5da81bc
Emmett McDow <emmett.mcdow@gmail.com>
2024-08-11 11:11:49 -0700

    Adds TLS to proxy server

--- server/main.go
+++ server/main.go
@@ -14,13 +14,16 @@ func main() {
 	wg.Add(1)
 	controllerServer := controller(wg, 5678, true)
 	wg.Add(1)
-	proxyServer := proxy(wg, 1234)
+	proxyServer := proxy(wg, 80, false)
+	wg.Add(1)
+	proxyServerTLS := proxy(wg, 443, true)
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	go func() {
 		// TODO: figure out this TODO business with the context
 		defer proxyServer.Shutdown(context.TODO())
+		defer proxyServerTLS.Shutdown(context.TODO())
 		defer controllerServer.Shutdown(context.TODO())
 		for sig := range c {
 			if sig == syscall.SIGINT {
